55a1c4eae3948a25c76567c167e71f46418aa9c3
Rimaz Khan <rimazk@umich.edu>
2020-07-12 10:55:29 -0400

    add intro message

--- boatymcbotface/messaging.py
+++ boatymcbotface/messaging.py
@@ -1,20 +1,25 @@
 from slackeventsapi import SlackEventAdapter
 import os
+from emoji import emojize
 from . import app
-from .utils import send_message
+from .utils import send_message, send_picture
 
-
+INTRO_MESSAGE = '''
+    Boats. You need em, I got em. What else needs to be said? 
+    Check out my code here :point_right: :sunglasses: :point_right: 
+    https://github.com/rimazk123/boatymcbotface
+    '''
 SIGNING_SECRET = os.environ['SIGNING_SECRET']
 slack_events_adapter = SlackEventAdapter(SIGNING_SECRET, "/slack/events", app)
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    r = send_message('bottesting').json()
-    print(r)
-    return "message sent"
-
-
 @slack_events_adapter.on("app_mention")
 def handle_message(event_data):
-    send_message(event_data['event']['channel'])
+    msg = event_data['event']['text']
+    channel = event_data['event']['channel']
+
+    if 'introduce yourself' in msg:
+        send_message(emojize(INTRO_MESSAGE), channel)
+    else:
+        send_message('BOATS!!', channel)
+        send_picture(event_data['event']['channel'])
--- boatymcbotface/utils.py
+++ boatymcbotface/utils.py
@@ -2,14 +2,14 @@ import requests
 import random
 import os
 
-URL = "https://www.reddit.com/r/boats.json"
+BOATS_URL = "https://www.reddit.com/r/boats.json"
 SLACK_URL = "https://slack.com/api/chat.postMessage"
 headers = {'User-agent': 'rimaz\'s bot'}
 slack_access_token = os.environ['ACCESS_TOKEN']
 
 
 def get_link():
-    r = requests.get(URL, headers=headers)
+    r = requests.get(BOATS_URL, headers=headers)
     body = r.json()
 
     while True:
@@ -21,14 +21,18 @@ def get_link():
             return url
 
 
-def send_message(channel):
+def send_message(message, channel):
     headers = {
         "Authorization": "Bearer " + slack_access_token,
         "Content-Type": "application/json"
     }
     data = {
         "channel": channel,
-        "text": get_link()
+        "text": message
     }
 
     return requests.post(SLACK_URL, json=data, headers=headers)
+
+
+def send_picture(channel):
+    return send_message(get_link(), channel)
