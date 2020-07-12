fd9c4f2c56e4e18936aa63c51ec9fa78fe26ceed
Rimaz Khan <rimazk@umich.edu>
2020-07-12 11:50:26 -0400

    add scheduler

--- boatymcbotface/__init__.py
+++ boatymcbotface/__init__.py
@@ -4,3 +4,4 @@ app = Flask(__name__)
 
 import boatymcbotface.messaging
 import boatymcbotface.utils
+import boatymcbotface.scheduler
---
+++ boatymcbotface/scheduler.py
@@ -0,0 +1,19 @@
+import schedule
+from .messaging import send_picture
+import threading
+
+
+def job():
+    print("running job")
+    send_picture("bottesting")
+
+
+def run_job():
+    schedule.every().sunday.at("12:00").do(job)
+    while True:
+        schedule.run_pending()
+
+
+t = threading.Thread(target=run_job)
+t.daemon = True
+t.start()
